2bd04d558f31e7ce1df366a9ac7200939eba6709
Zekai Lai <zlai7@cs.brown.edu>
2023-05-11 12:18:58 -0400

    May-11-2023-test

--- kernel/vm/brk.c
+++ kernel/vm/brk.c
@@ -56,67 +56,67 @@
  */
 long do_brk(void *addr, void **ret)
 {
-    // If addr is not in valid range 
-    if((size_t)addr>USER_MEM_HIGH||addr<curproc->p_start_brk){
-        return -ENOMEM;
-    }
-    // If addr is NULL
-    if(addr==NULL){
-        *ret=curproc->p_brk;
-        return 0;
-    }
-    // TODO: How to consider to page align for the start brk
+    // // If addr is not in valid range 
+    // if((size_t)addr>USER_MEM_HIGH||addr<curproc->p_start_brk){
+    //     return -ENOMEM;
+    // }
+    // // If addr is NULL
+    // if(addr==NULL){
+    //     *ret=curproc->p_brk;
+    //     return 0;
+    // }
+    // // TODO: How to consider to page align for the start brk
     
-    // We need to Initialize a new heap
-    if(curproc->p_brk==curproc->p_start_brk){
-        size_t start_pn=ADDR_TO_PN(PAGE_ALIGN_UP(curproc->p_start_brk));
-        size_t end_pn=ADDR_TO_PN(PAGE_ALIGN_UP(addr));
+    // // We need to Initialize a new heap
+    // if(curproc->p_brk==curproc->p_start_brk){
+    //     size_t start_pn=ADDR_TO_PN(PAGE_ALIGN_UP(curproc->p_start_brk));
+    //     size_t end_pn=ADDR_TO_PN(PAGE_ALIGN_UP(addr));
 
-        if(end_pn==start_pn){
-            return -ENOMEM;
-        }else{
-            if(!vmmap_is_range_empty(curproc->p_vmmap,start_pn,end_pn-start_pn)){
-                vmmap_remove(curproc->p_vmmap,start_pn,end_pn-start_pn);
-            }
+    //     if(end_pn==start_pn){
+    //         return -ENOMEM;
+    //     }else{
+    //         if(!vmmap_is_range_empty(curproc->p_vmmap,start_pn,end_pn-start_pn)){
+    //             vmmap_remove(curproc->p_vmmap,start_pn,end_pn-start_pn);
+    //         }
 
-            vmarea_t *new_vm;
-            long tmp=vmmap_map(curproc->p_vmmap,NULL,start_pn,end_pn-start_pn,PROT_READ|PROT_WRITE, 
-            MAP_PRIVATE|MAP_ANON | MAP_FIXED,0,VMMAP_DIR_HILO,&new_vm);
+    //         vmarea_t *new_vm;
+    //         long tmp=vmmap_map(curproc->p_vmmap,NULL,start_pn,end_pn-start_pn,PROT_READ|PROT_WRITE, 
+    //         MAP_PRIVATE|MAP_ANON | MAP_FIXED,0,VMMAP_DIR_HILO,&new_vm);
 
-            if(tmp<0){
-                return tmp;
-            }
-        }
-        curproc->p_brk=addr;
-    } else if(addr<curproc->p_brk){
-        // The heap this to shrink
-        size_t new_end=ADDR_TO_PN(PAGE_ALIGN_UP(addr)); // Get the start page number
-        size_t old_end=ADDR_TO_PN(PAGE_ALIGN_UP(curproc->p_brk));    // Get the end page number
+    //         if(tmp<0){
+    //             return tmp;
+    //         }
+    //     }
+    //     curproc->p_brk=addr;
+    // } else if(addr<curproc->p_brk){
+    //     // The heap this to shrink
+    //     size_t new_end=ADDR_TO_PN(PAGE_ALIGN_UP(addr)); // Get the start page number
+    //     size_t old_end=ADDR_TO_PN(PAGE_ALIGN_UP(curproc->p_brk));    // Get the end page number
 
-        // The page number may be the same
-        if(new_end!=old_end){
-            vmmap_remove(curproc->p_vmmap,new_end,old_end-new_end);    // Clean up the specified range
-        }
-        curproc->p_brk=addr; // Update the new end of the heap
-    } else if(addr>curproc->p_brk){
-        size_t old_end=ADDR_TO_PN(PAGE_ALIGN_UP(curproc->p_brk));
-        size_t new_end=ADDR_TO_PN(PAGE_ALIGN_UP(addr));
+    //     // The page number may be the same
+    //     if(new_end!=old_end){
+    //         vmmap_remove(curproc->p_vmmap,new_end,old_end-new_end);    // Clean up the specified range
+    //     }
+    //     curproc->p_brk=addr; // Update the new end of the heap
+    // } else if(addr>curproc->p_brk){
+    //     size_t old_end=ADDR_TO_PN(PAGE_ALIGN_UP(curproc->p_brk));
+    //     size_t new_end=ADDR_TO_PN(PAGE_ALIGN_UP(addr));
 
-        if(old_end!=new_end){
-            if(!vmmap_is_range_empty(curproc->p_vmmap,old_end,new_end-old_end)){
-                return -ENOMEM;     // Beyond its valid range
-            }
-            // vmarea_t *cur_vma=vmmap_lookup(curproc->p_vmmap,);
-        }
+    //     if(old_end!=new_end){
+    //         if(!vmmap_is_range_empty(curproc->p_vmmap,old_end,new_end-old_end)){
+    //             return -ENOMEM;     // Beyond its valid range
+    //         }
+    //         // vmarea_t *cur_vma=vmmap_lookup(curproc->p_vmmap,);
+    //     }
         
 
-        curproc->p_brk=addr;
+    //     curproc->p_brk=addr;
         
-        // vmarea_t *cur_vma=vmmap_lookup();
+    //     // vmarea_t *cur_vma=vmmap_lookup();
 
-    }
-    *ret=curproc->p_brk;
-    // TODO: Need to come back
-    // NOT_YET_IMPLEMENTED("VM: do_brk");
-    return 0;
+    // }
+    // *ret=curproc->p_brk;
+    // // TODO: Need to come back
+    // // NOT_YET_IMPLEMENTED("VM: do_brk");
+    // return 0;
 }
